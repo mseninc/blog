63ecc93ee43cfb771984e945340fdf44ced99d97
Kenji YAMADA <kenzauros@users.noreply.github.com>
2021-10-25 18:16:17 +0900

    Update start.sh

--- start.sh
+++ start.sh
@@ -13,14 +13,23 @@ do
   fi
 done
 
-echo -n "slug? > "
-read SLUG
-
-if [ -z "${SLUG}" ]; then
-  echo 'CANCELED'
-  exit 0
+BRANCH_NAME=`git rev-parse --abbrev-ref HEAD`
+if [ "`echo $BRANCH_NAME | grep '^post/'`" ]; then
+  SLUG_CANDIDATE=`echo $BRANCH_NAME | sed -e 's%^post/%%'`
 fi
 
+read -p "slug? [$SLUG_CANDIDATE]> " SLUG
+
+if [ -z "${SLUG}" ]; then
+  SLUG=$SLUG_CANDIDATE
+fi
+
+if [ "`echo $SLUG | grep '[^0-9a-zA-Z-]'`" ]; then
+  echo "ERROR: slug can contain only alphanumeric characters and hyphen (0-9, a-z, -)" 1>&2
+  exit 1
+fi
+
+SLUG="${SLUG,,}" # make slug lower-case
 YEAR=`date '+%Y'`
 DIR="${AUTHOR}/${YEAR}/${SLUG}"
 IMAGES="${DIR}/images"
@@ -40,17 +49,29 @@ case $CONFIRM in
     mkdir -p ${IMAGES}
     cat <<EOF > "${MD}"
 ---
-title: 
-date: 
+title:
+date:
 author: ${AUTHOR}
 tags: []
-description: 
+description:
 ---
 
 EOF
     ;;
   * )
     echo 'CANCELED'
+    exit 0
+    ;;
+esac
+
+echo -n "Open with code? [y/N] > "
+read EDITOR
+
+case $EDITOR in
+  [Yy]* )
+    code ${MD}
+    ;;
+  * )
     exit 0
     ;;
 esac
