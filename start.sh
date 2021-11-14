e08084ec787450d0fbc907bc0889700682f114ec
Kenji YAMADA <kenzauros@users.noreply.github.com>
2021-11-14 11:15:14 +0900

    start.sh を改良 (#52)
    * Update start.sh
    
    * Update start.sh
    
    * `start.sh` の変更点を追加
    
    * 助詞を修正
    
    Co-authored-by: Kenji YAMADA <kenzauros@users.noreply.github.com>
    
    Co-authored-by: Kiyoshi KOYAMA <koyama@msen.jp>

--- start.sh
+++ start.sh
@@ -13,14 +13,28 @@ do
   fi
 done
 
-echo -n "slug? > "
-read SLUG
-
-if [ -z "${SLUG}" ]; then
-  echo 'CANCELED'
-  exit 0
+BRANCH_NAME=`git rev-parse --abbrev-ref HEAD`
+if [ "`echo $BRANCH_NAME | grep '^post/'`" ]; then
+  SLUG_CANDIDATE=`echo $BRANCH_NAME | sed -e 's%^post/%%'`
 fi
 
+read -p "slug? [$SLUG_CANDIDATE]> " SLUG
+
+if [ -z "${SLUG}" ]; then
+  if [ -n "${SLUG_CANDIDATE}" ]; then
+    SLUG=$SLUG_CANDIDATE
+  else
+    echo 'CANCELED'
+    exit 0
+  fi
+fi
+
+if [ "`echo $SLUG | grep '[^0-9a-zA-Z-]'`" ]; then
+  echo "ERROR: slug can contain only alphanumeric characters and hyphen (0-9, a-z, -)" 1>&2
+  exit 1
+fi
+
+SLUG="${SLUG,,}" # make slug lower-case
 YEAR=`date '+%Y'`
 DIR="${AUTHOR}/${YEAR}/${SLUG}"
 IMAGES="${DIR}/images"
@@ -51,6 +65,18 @@ EOF
     ;;
   * )
     echo 'CANCELED'
+    exit 0
+    ;;
+esac
+
+echo -n "Open with code? [y/N] > "
+read EDITOR
+
+case $EDITOR in
+  [Yy]* )
+    code ${MD}
+    ;;
+  * )
     exit 0
     ;;
 esac
